25ce0cbe67551086231a6498d12f92c915f9a25a
levisilvaaa <levisilvasanthoss@gmail.com>
2025-11-06 19:23:53 -0300

    Updated index.html

--- src/config/videoConfig.ts
+++ src/config/videoConfig.ts
@@ -10,8 +10,8 @@ export const AVAILABLE_VIDEOS: Record<string, VideoConfig> = {
   bakingSoda: {
     id: 'bakingSoda',
     name: 'Baking Soda Cures Impotence',
-    embedUrl: 'https://scripts.converteai.net/f5ab9e88-cc1b-4dce-a537-c7de7e019d8b/players/68fbc04606facf31533282a0/v4/embed.html',
-    playerId: '68fbc04606facf31533282a0',
+    embedUrl: 'https://scripts.converteai.net/f5ab9e88-cc1b-4dce-a537-c7de7e019d8b/players/690d149c52aad814cc4b2d03/v4/embed.html',
+    playerId: '690d149c52aad814cc4b2d03',
     showDTCAtSeconds: 2655
   }
 };
